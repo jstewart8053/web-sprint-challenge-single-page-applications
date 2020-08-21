7e79954c58bdb6559b0962e19316c9aa48ef51ca
Jennifer Stewart <jstewart8053@gmail.com>
2020-08-21 12:56:29 -0500

    project finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css'
 import Home from "./Home";
 import Order from './Order'
 import Nav from "./Nav";
+import * as yup from 'yup'
+import axios from 'axios'
 
 
-const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
 
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/pizza'>
-            <Order />
-          </Route>
-        </Switch>
-
-      </div>
-    </Router>
-  );
-};
-export default App;
+let schema = yup.object().shape({
+    size: yup
+      .string()
+      .required('Please select a size'),
+    sauce: yup
+      .string()
+      .required('Please select a sauce'),
+    toppings: yup
+      .string()
+      .required('Please enter a password'),
+    specialInstructions: yup
+      .string()
+  });
+  
+  const formData = {
+    size: '',
+    sauce: '',
+    toppings: '',
+    specialInstructions: '',
+  }
+  
+  const App = () => {
+    const [inputValue, setInputValue] = useState(formData);
+    const [pizza, setPizza] = useState([])
+    const [formErrors, setFormErrors] = useState([]);
+  
+  
+    const formSubmit = (event) => {
+      event.preventDefault();
+  
+      schema.validate(inputValue)
+        .then(valid => {
+          setInputValue(inputValue)
+          axios.post('https://reqres.in/api/users', { inputValue })
+            .then(res => {
+              setPizza([...pizza, res.data.inputValue])
+              setInputValue(formData)
+            })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  
+  
+    }
+  
+  
+    const formChange = (name, value) => {
+      yup.reach(schema, name).validate(value)
+        .then(msg => {
+          setFormErrors({ ...formErrors, [name]: '' })
+        })
+        .catch(err => {
+          setFormErrors({ ...formErrors, [name]: err.errors[0] })
+        })
+  
+  
+      setInputValue({
+        ...inputValue,
+        [name]: value,
+      })
+      console.log(inputValue)
+    }
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+  
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/pizza'>
+              <Order submit={formSubmit} updateInput={formChange}
+                errors={formErrors} pizza={pizza}
+                value={inputValue}
+              />
+            </Route>
+          </Switch>
+  
+        </div>
+      </Router>
+    );
+  };
+  export default App;
--- src/Order.js
+++ src/Order.js
@@ -1,50 +1,63 @@
 import React from 'react'
 
-function Order() {
-  return (
-    <div className='order'>
-      <h1>Build your own pizza</h1>
-      <form>
-        <div className="item">
-          <label htmlFor="size">Pick a size</label>
-          <select name="size">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-          </select>
-        </div>
-        <div className="item">
-          <label htmlFor="sauce">Pick your sauce:</label>
-          Red
-          <input type="radio" name="sauce" value='redSauce' />
-          White
-          <input type="radio" name="sauce" value='redSauce' />
-          BBQ
-          <input type="radio" name="sauce" value='redSauce' />
-        </div>
+function Order(props) {
+    const { submit, updateInput, errors, pizza } = props
 
-        <div className="item">
-          <label>Toppings</label>
-          Pepperoni: <input type="checkbox" name="pepperoni"
-            value="pepperoni" />
-
-          Mushrooms: <input type="checkbox" name="mushrooms" value="mushrooms" />
-
-          Sausage: <input type="checkbox" name="sausage"
-            value="sausage" />
-
-          Bacon: <input type="checkbox" name="bacon" value="bacon" />
-
-          <div className="item">
-            <label htmlFor="specialInstructions">Special Instructions</label>
-            <textarea name="specialInstructions" cols="30" rows="3"></textarea>
-          </div>
-
-          <button>Submit  </button>
-        </div>
-      </form>
-    </div>
-  )
-}
-
-export default Order
+    const change = (event) => {
+      const { name, value } = event.target;
+      updateInput(name, value)
+    }
+      return (
+           <div className='order'>
+              <div>
+                {errors.size}<br />
+                {errors.sauce}<br />
+                {errors.toppings}
+              </div>
+              <h1>Build your own pizza</h1>
+              <form onSubmit={submit}>
+                <div className="item">
+                  <label htmlFor="size">Pick a size</label>
+                  <select name="size" onChange={change}>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                  </select>
+                </div>
+                <div className="item">
+                  <label htmlFor="sauce">Pick your sauce:</label>
+                  Red
+                  <input type="radio" name="sauce" value='red' onChange={change} />
+                  White
+                  <input type="radio" name="sauce" value='white' onChange={change} />
+                  BBQ
+                  <input type="radio" name="sauce" value='bbq' onChange={change} />
+                </div>
+        
+                <div className="item">
+                  <label>Toppings</label>
+                  Pepperoni: <input type="checkbox" name="toppings"
+                    value="pepperoni" onChange={change} />
+        
+                  Mushrooms: <input type="checkbox" name="toppings" value="mushrooms" onChange={change} />
+        
+                  Sausage: <input type="checkbox" name="toppings"
+                    value="sausage" onChange={change} />
+        
+                  Bacon: <input type="checkbox" name="toppings" value="bacon" onChange={change} />
+        
+                  <div className="item">
+                    <label htmlFor="specialInstructions">Special Instructions</label>
+                    <textarea name="specialInstructions" cols="30" rows="3" onChange={change}></textarea>
+                  </div>
+        
+                  <button>Submit  </button>
+                </div>
+              </form>
+        
+              <div>{pizza.size}</div>
+            </div>
+          )
+        }
+        
+        export default Order
